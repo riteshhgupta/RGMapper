42dbb6c034f54e647df7088257adcfc9d6ef62b1
Ritesh <rg.riteshh@gmail.com>
2017-02-02 19:02:20 +0530

    chore(*): project init

---
+++ RGMapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGMapper
+//
+//  Created by Ritesh Gupta on 02/02/17.
+//  Copyright © 2017 Ritesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
