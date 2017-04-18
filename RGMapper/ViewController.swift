1c7136a03d436950e2be95afae8099ce1d2faf30
Ritesh <rg.riteshh@gmail.com>
2017-04-18 23:16:39 +0530

    chore(*): rearranged files

--- RGMapper/ViewController.swift
+++ RGMapper/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
+struct User {
+	
+	let name: String
+	let age: Int
+}
+
+extension User: Mappable {
+	
+	static func map(_ value: Any?) throws -> User {
+		guard let json = value as? [String: Any] else {
+			throw MappableError.unableToParse("root type mismatch")
+		}
+		return try User(
+			name: json["name"]^^,
+			age: json["age"]^^
+		)
+	}
+}
+
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.		
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		let dict: [String: Any] = ["name": "Ritesh", "age": "25"]
+		do {
+			let user: User = try dict^^
+			print(user)
+		} catch {
+			print(error.localizedDescription)
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
 
